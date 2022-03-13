724906643f7ed2d035b2a9e6d9b43d05e603e0b8
MrZorg111 <yodji333@mail.ru>
2022-03-13 20:48:17 +0300

    Good finish

--- main.cpp
+++ main.cpp
@@ -1,42 +1,15 @@
 #include <iostream>
 #include <vector>
-std::vector<int> expansion(std::vector<int> vec, int a) {
-    std::vector<int> temp(vec.size() + 1);
-    for ( int r = 0; r < temp.size(); r++) {
-        temp[r] = vec[r];
-        temp[vec.size() + 1]  = a;
-    }
-}
 
 int main() {
-    //С клавиатуры вводятся числа.
-    // Когда пользователь вводит -1 -- необходимо выводить на экран пятое по возрастанию число среди введённых.
-    // Когда пользователь вводит -2 -- программа завершает работу.
-    //
-    //Пример:
-    //
-    //ввод: 7 5 3 1 2 4 6 -1
-    //
-    //вывод: 5 (в отсортированном виде массив выглядит так: {1,2,3,4,5,6,7})
-    //
-    //ввод: 1 1 1 -1
-    //
-    //вывод: 2 (в отсортированном виде массив выглядит так: {1,1,1,1,2,3,4,5,6,7})
-    //
-    //ввод -2
-    //
-    //завершение программы
     int n = 0;
-   std::vector<int> vec(n);
-    for (int i = -1; i < vec.size(); i++) {
+    std::vector<int> vec(n);
+    do {
         std::cout << "Enter number: ";
         std::cin >> n;
-        if (n == -2) {
-            std::cout << "Completion of the program.";
-            return 0;
-        }
         if (n != -1) {
-            vec[i] = expansion(vec(), n);
+            vec.resize(vec.size() + 1);
+            vec[vec.size() - 1] = n;
         }else {
             for (int t = 0; t < vec.size(); t++) {
                 for (int j = 0; j < vec.size() - t - 1; j++) {
@@ -48,11 +21,7 @@ int main() {
                 }
             }
            std::cout << vec[4] << "\n";
-            for (int a = 0; a < vec.size(); a++) {
-                std::cout << vec[a] << " ";
-            }
         }
-
-    }
-
+    }while (n != - 2);
+    std::cout << "Completion of the program.";
 }
